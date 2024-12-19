bf2d9d0ee329f09f755f6e8459ae86ddd3e515ba
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-12-19 20:28:06 +0100

    update gi.py

--- bot/workers/handlers/gi.py
+++ bot/workers/handlers/gi.py
@@ -630,6 +630,7 @@ async def random_challenge(event, args, client):
         caption += "\n\n*Rules:*"
         caption += "\n*1.* Characters can only be substituted for another when you don't have that character."
         caption += "\n*2.* Only a character can be substituted. if you don't have two or more of the randomized characters, try again."
+        caption += "\n*3.* The substitute must share the same element and the same or lower rarity as the substituted character."
         caption += "\n*Good luck!*"
         await clean_reply(event, reply, "reply_photo", photo=final_img, caption=caption)
     except Exception:
