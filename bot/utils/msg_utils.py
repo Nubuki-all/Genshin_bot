c0b5bb9986b3a039fb59882efc021b22df8148f8
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-12-25 12:04:43 +0100

    update msg_utils.py

--- bot/utils/msg_utils.py
+++ bot/utils/msg_utils.py
@@ -210,7 +210,7 @@ class Event:
         self,
         video: str | bytes,
         caption: str = None,
-        quote: bool = True
+        quote: bool = True,
         viewonce: bool = False,
     ):
         quoted = self.message if quote else None
