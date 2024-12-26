5f5368fde4df26f42392bd447b0e83e4394a4c66
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-12-26 21:57:04 +0100

    fix

--- bot/utils/os_utils.py
+++ bot/utils/os_utils.py
@@ -36,7 +36,7 @@ def updater(msg=None):
             ver = file.read()
         qclean()
         Path("update").touch()
-        bashrun(["python3", "update.py"])
+        bashrun([sys.executable, "update.py"])
         with open(version_file, "r") as file:
             ver2 = file.read()
 
