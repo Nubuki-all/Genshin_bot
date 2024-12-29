add2c5ede407a27335388ed216620bde2eceb7e6
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-12-29 19:00:18 +0100

    still debugging

--- bot/utils/msg_utils.py
+++ bot/utils/msg_utils.py
@@ -342,7 +342,6 @@ def register(key: str | None = None):
 bot.register = register
 background_tasks = set()
 
-
 async def on_message(client: NewAClient, message: MessageEv):
     event = construct_event(message)
     if event.type == "text":
@@ -353,7 +352,8 @@ async def on_message(client: NewAClient, message: MessageEv):
         )
         func = function_dict.get(command)
         if func:
-            task = asyncio.create_task(func(client, event))
+            loop = asyncio.new_event_loop()
+            task = loop.create_task(func(client, event))
             background_tasks.add(task)
             task.add_done_callback(background_tasks.discard)
     for func in function_dict[None]:
