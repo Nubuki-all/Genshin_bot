0ca872e3faf0b265838d24743ef3033fa8f9119a
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-01-04 22:59:23 +0000

    pyLint: auto-fixes

--- bot/utils/msg_utils.py
+++ bot/utils/msg_utils.py
@@ -84,7 +84,9 @@ class Event:
             if (
                 self.quoted.quotedMessage.documentWithCaptionMessage.message.documentMessage.URL
             ):
-                self.quoted_document = self.quoted.quotedMessage.documentWithCaptionMessage
+                self.quoted_document = (
+                    self.quoted.quotedMessage.documentWithCaptionMessage
+                )
             if self.quoted.quotedMessage.documentMessage.URL:
                 self.quoted_document = self.quoted.quotedMessage.documentMessage
             self.quoted_image = (
