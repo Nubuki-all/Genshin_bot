50e8296882b348f4986f95225c3db1a17e32349a
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-01-05 10:01:05 +0100

    bug fixes

--- bot/workers/handlers/wa.py
+++ bot/workers/handlers/wa.py
@@ -451,6 +451,10 @@ async def delete_notes(event, args, client):
         if not (notes := bot.notes_dict.get(chat)):
             return await event.reply(f"_No notes found for chat:_ *{chat_name}*!")
         if args.casefold() == "all":
+            if not user_is_owner(user) and event.chat.is_group:
+                return await event.reply(
+                f"*Permission denied.*"
+                )
             bot.notes_dict.pop(chat)
             await save2db2(bot.notes_dict, "note")
             return await event.reply(
