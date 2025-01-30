75417813f4bcbb8c034b3d5aef5ae5c06d71ee09
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-01-31 00:33:47 +0100

    Additional fixes

--- bot/workers/handlers/gi.py
+++ bot/workers/handlers/gi.py
@@ -246,7 +246,7 @@ async def enka_handler(event, args, client):
             for name in cards.split(","):
                 name = name.strip()
                 info = await get_gi_info(query=name)
-                info = await get_character_info_fallback(card) if not info else info
+                info = await get_character_info_fallback(name) if not info and card.casefold() != "traveler" else info
                 if not info:
                     errors += f"{name}, "
                     continue
