79271f97a16a5d75103a2de594a2d53b9ca45180
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-01-31 00:21:21 +0100

    bug fixes

--- bot/utils/msg_utils.py
+++ bot/utils/msg_utils.py
@@ -79,9 +79,9 @@ class Event:
         # To do expand quoted; has members [stanzaID, participant,
         # quotedMessage.conversation]
         self.quoted = self.ext_msg.contextInfo if add_replied else None
-        self.quoted_document = self.quoted_image = self.quoted_video = (
-            self.quoted_viewonce
-        ) = None
+        self.quoted_audio = self.quoted_document = self.quoted_image = (
+            self.quoted_video
+        ) = self.quoted_viewonce = None
         if self.quoted:
             if self.quoted.quotedMessage.audioMessage.ByteSize():
                 self.quoted_audio = self.quoted.quotedMessage.audioMessage
