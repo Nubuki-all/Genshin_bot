4ebb516bc0a2bb155e641973d55d58518f146a48
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-02-21 05:03:09 +0100

    bug fixes

--- bot/utils/gi_utils.py
+++ bot/utils/gi_utils.py
@@ -110,7 +110,7 @@ async def get_enka_card(uid, char_id, akasha=True, huid=False, template=1, retry
             return
         await logger(e="Updating enka assets and trying again…")
         await enka_update()
-        return await get_enka_card(
+        result, error = await get_enka_card(
             uid, char_id, akasha=True, huid=False, template=1, retry=True
         )
     except Exception as e:
