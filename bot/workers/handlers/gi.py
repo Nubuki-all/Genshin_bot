0328d926fae66a35899409aa9f43896c3004cc35
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-03-30 19:51:51 +0100

    update gi.py

--- bot/workers/handlers/gi.py
+++ bot/workers/handlers/gi.py
@@ -635,10 +635,7 @@ async def get_events(event, args, client):
         # Get Upcoming Events
         items = tables[1].find_all("td")
         for item in items:
-            if value := item.find("a"):
-                if value.get("title"):
-                    temp_dict.update(name=value.getText())
-            elif value := item.find("img"):
+            if (value := item.find("img")) or ((value := item.find("a")) and value.get("title")):
                 temp_dict.update({"name": value.get("alt") or item.getText()})
                 link = value.get("src", str())
                 if link.startswith("data"):
