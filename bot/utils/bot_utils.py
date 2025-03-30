9975bd9eab17e18551c53e5599afe2126ec8dea4
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-03-30 12:17:48 +0100

    update bot_utils.py

--- bot/utils/bot_utils.py
+++ bot/utils/bot_utils.py
@@ -136,7 +136,7 @@ def get_date(value, start=True):
             if len(value.split()[1]) == 8:
                 index = 10
                 add_v = str()
-           else:
+            else:
                index = 19
                add_v = " 00:00:00"
             return valve[index:] + add_v
